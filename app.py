473d9ee46cb25e3566feaf26c85faec7d0d548d0
John Chong <johnchong.99.17@gmail.com>
2023-04-27 12:55:17 -0500

    start pt 2

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 
 import os
 
-from flask import Flask, request, redirect, render_template
-from models import connect_db, User, db
+from flask import Flask, request, redirect, render_template, flash
+from models import connect_db, User, Post, db, DEFAULT_IMAGE_URL
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_debugtoolbar import DebugToolbarExtension
@@ -49,9 +49,8 @@ def process_add_user():
 
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    print(f"REQUEST FORM: {request.form}")
-    image_url = request.form['image_url']
-# TODO: update default image
+    image_url = request.form.get('image_url',DEFAULT_IMAGE_URL)
+
     new_user = User(first_name = first_name, last_name=last_name, image_url=image_url)
     db.session.add(new_user)
     db.session.commit()
@@ -63,8 +62,12 @@ def render_user_details(user_id):
     """render the details page for a single user"""
 
     user = User.query.get(user_id)
+    author = User.query.get(user_id)
+    posts = author.posts
 
-    return render_template('user_detail.html', user=user )
+    return render_template('user_detail.html',
+                           user=user,
+                           posts=posts)
 
 @app.get('/users/<int:user_id>/edit')
 def render_edit_user(user_id):
@@ -91,10 +94,35 @@ def process_user_edit(user_id):
 @app.post('/users/<int:user_id>/delete')
 def delete_user(user_id):
     """ handles deleting user """
-# TODO: always use get_or_404
+
     user = User.query.get_or_404(user_id)
 
     db.session.delete(user)
     db.session.commit()
 
     return redirect('/users')
+
+@app.get('/users/<int:user_id>/posts/new')
+def new_post_page(user_id):
+    """show new post page"""
+
+    user = User.query.get_or_404(user_id)
+
+    return render_template('new_post.html',
+                           user=user)
+
+@app.post('/users/<int:user_id>/posts/new')
+def added_new_post(user_id):
+    """adding new post"""
+
+    user = User.query.get_or_404(user_id)
+    title = request.form['title']
+    content = request.form['content']
+
+    new_post = Post(title=title, content=content, user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+
+    flash("New post added!")
+
+    return redirect(f"/users/{user_id}")
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import URLType
+from datetime import datetime
 
 DEFAULT_IMAGE_URL = None
 
@@ -39,3 +40,37 @@ class User(db.Model):
         URLType,
         default=DEFAULT_IMAGE_URL
     )
+
+
+class Post(db.Model):
+    """Post"""
+    __tablename__ = 'posts'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    title = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    content = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.now
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False
+    )
+
+    user = db.relationship('User', backref='posts')
